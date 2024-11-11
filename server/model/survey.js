7e32f0f63f7e930d75809f72f71566a57e96314c
Ashvien29 <ashvien.rameshkumar@ontariotechu.net>
2024-11-10 20:19:09 -0500

    Adding comments

--- server/model/survey.js
+++ server/model/survey.js
@@ -1,7 +1,7 @@
 //const { Collection, default: mongoose } = require("mongoose");
 
 const mongoose = require("mongoose");
-
+/*Creating the model for the surveys */
 let surveyModel = mongoose.Schema({
     Name: String,
     JobTitle: String,
@@ -14,3 +14,4 @@ let surveyModel = mongoose.Schema({
     collection:"Job_surveys"
 });
 module.exports =mongoose.model('Survey',surveyModel);
+/*modlue exports equal to mongoose model for Survey model*/ 
